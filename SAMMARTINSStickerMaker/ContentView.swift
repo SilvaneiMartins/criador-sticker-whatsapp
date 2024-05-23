dae241288100f153745b5b14c2c31b3298c52644
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2024-05-23 10:35:46 -0400

    Initial Commit

---
+++ SAMMARTINSStickerMaker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SAMMARTINSStickerMaker
+//
+//  Created by Silvanei  Martins on 23/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SAMMARTINSStickerMaker/SAMMARTINSStickerMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAMMARTINSStickerMakerApp.swift
+//  SAMMARTINSStickerMaker
+//
+//  Created by Silvanei  Martins on 23/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SAMMARTINSStickerMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
